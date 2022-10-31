0b8e20a6e9d4aaa112ad19a9050f90a02ade1dad
Mayank <mayankkk@gmail.com>
2022-10-31 15:27:16 +0530

    bashscript validatations

--- vpn-bash/vpn-rsa.sh
+++ vpn-bash/vpn-rsa.sh
@@ -1,48 +1,60 @@
+servername=manthanserver           #Can be changed according to user requirement
 if (($# >= 2)); 
 then    
     DIR="$( cd "$( dirname "$0" )" && pwd )"
     echo $DIR
     #ServerFile Creation
     cd $DIR
-    if aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep server;
-    then 
+    if aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $servername;
+    then
+        aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $servername | cut -f1 > server_arn
         echo "Server Certificate Already Uploaded to ACM"
+        git clone https://github.com/OpenVPN/easy-rsa.git
     else
-        cd $DIR
         git clone https://github.com/OpenVPN/easy-rsa.git 
 
         ./easy-rsa/easyrsa3/easyrsa init-pki
         cp -R $DIR/template/. pki/
         cp $DIR/template/easyrsa ./easy-rsa/easyrsa3/easyrsa
         ./easy-rsa/easyrsa3/easyrsa build-ca nopass
-        ./easy-rsa/easyrsa3/easyrsa build-server-full server nopass
+        ./easy-rsa/easyrsa3/easyrsa build-server-full $servername nopass
         mkdir acm
         cp pki/ca.crt acm
-        cp pki/issued/server.crt acm
-        cp pki/private/server.key acm
-        aws acm import-certificate --certificate fileb://acm/server.crt --private-key fileb://acm/server.key --certificate-chain fileb://acm/ca.crt
+        cp pki/issued/$servername.crt acm
+        cp pki/private/$servername.key acm
+        aws acm import-certificate --certificate fileb://acm/$servername.crt --private-key fileb://acm/$servername.key --certificate-chain fileb://acm/ca.crt
     fi
 
     #Client ADD or DELETE
 
     if [ $2 = "ADD" ]; then
-        ./easy-rsa/easyrsa3/easyrsa build-client-full $1 nopass
-        cp pki/issued/$1.crt acm
-        cp pki/private/$1.key acm
-        aws acm import-certificate --certificate fileb://acm/$1.crt --private-key fileb://acm/$1.key --certificate-chain fileb://acm/ca.crt
-        echo 'User added'
+        if aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $1;
+        then 
+            echo "Client with same name already exists on ACM"
+            exit 1
+        else
+            ./easy-rsa/easyrsa3/easyrsa build-client-full $1 nopass
+            cp pki/issued/$1.crt acm
+            cp pki/private/$1.key acm
+            aws acm import-certificate --certificate fileb://acm/$1.crt --private-key fileb://acm/$1.key --certificate-chain fileb://acm/ca.crt
+            echo 'User added'
+        fi
     elif [ $2 = "DELETE" ]; then
-        ./easy-rsa/easyrsa3/easyrsa revoke $1
-        arn_to_delete=$(aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $1 | cut -f1)
-        aws acm delete-certificate --certificate-arn $arn_to_delete
-        echo 'User deleted'
+        if aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $1;
+        then
+            ./easy-rsa/easyrsa3/easyrsa revoke $1
+            arn_to_delete=$(aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $1 | cut -f1)
+            aws acm delete-certificate --certificate-arn $arn_to_delete
+            echo 'User deleted'
+        else
+            echo 'No client certificate with '$1
     else
         echo 'Enter a valid operation'
     fi
 
     cd ..
 
-    aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep server | cut -f1 > server_arn
+    aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $servername | cut -f1 > server_arn
     truncate -s-1 server_arn
     aws acm list-certificates --query CertificateSummaryList[].[CertificateArn,DomainName]   --output text | grep $1 | cut -f1 > client_arn
     truncate -s-1 client_arn
